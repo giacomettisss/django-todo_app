81af13d6caaa7d34bac0ae4004ff3d32f70552e1
Marcelo Giacometti da Silva <48400319+giacomettisss@users.noreply.github.com>
2021-04-01 23:53:17 -0300

    commit 2021040101
    Adding:
     - app about
     - app users
     - app tasks
     - settings some urls routes

---
+++ tasks/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def test(request):
+    return HttpResponse('Hello word!')
+
+
+def taskList(request):
+    return render(request, 'tasks/list.html')
+
+
+def yourName(request, name):
+    return render(request, 'tasks/yourname.html', {'name': name})
